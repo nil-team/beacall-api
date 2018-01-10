49b4ed483c0cd68f1e2b833ca396d7ff0689a7a7
Arnaud Manaranche <arnaud.mana@gmail.com>
2018-01-10 15:32:40 +0100

    make it better

--- Sources/beacall-api/main.swift
+++ Sources/beacall-api/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import Kitura
+import SwiftyJSON
+
+let router = Router()
+
+router.all("/", middleware: BodyParser(), StaticFileServer(path: "./Public"))
+
+router.get("/") { request, response, next in
+    
+    try response.send(["9h-11h": "Panoramas"]).end()
+}
+
+let port = 8080
+
+Kitura.addHTTPServer(onPort: port, with: router)
+Kitura.run()
