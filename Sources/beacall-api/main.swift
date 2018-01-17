fc54f4b050fd6a66ed4b9e1f26e0b1b5db96d13b
Arnaud Manaranche <arnaud.mana@gmail.com>
2018-01-17 10:17:06 +0100

    student1 + student2 json

--- Sources/beacall-api/main.swift
+++ Sources/beacall-api/main.swift
@@ -1,14 +1,33 @@
 import Kitura
-import SwiftyJSON
 
 let router = Router()
 
 router.all("/", middleware: BodyParser(), StaticFileServer(path: "./Public"))
 
-router.get("/") { request, response, next in
+router.get("/student1") { request, response, next in
     
-    try response.send(["9h-11h": "Panoramas"]).end()
+    try response.send([
+        "place": "Palais Brogniart",
+        "begin": "9h",
+        "end": "11h",
+        "lat": "48.8688356",
+        "long": "2.3414426",
+        "marge": "500"
+    ]).end()
 }
+
+router.get("/student2") { request, response, next in
+    
+    try response.send([
+        "place": "Les Panoramas",
+        "begin": "9h",
+        "end": "11h",
+        "lat": "48.870537",
+        "long": "2.342358",
+        "marge": "200"
+        ]).end()
+}
+
 
 let port = 8080
 
