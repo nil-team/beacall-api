b56d853c785936991f8088f07de2889d53dc9fe6
Arnaud Manaranche <arnaud.mana@gmail.com>
2018-01-17 22:17:31 +0100

    Server Heroku

--- Sources/beacall-api/main.swift
+++ Sources/beacall-api/main.swift
@@ -47,7 +47,17 @@ router.get("/student1") { request, response, next in
     try response.send(data).end()
 }
 
-let port = 8080
+// Resolve the port that we want the server to listen on.
+let port: Int
+let defaultPort = 8080
+if let requestedPort = ProcessInfo.processInfo.environment["PORT"] {
+    port = Int(requestedPort) ?? defaultPort
+} else {
+    port = defaultPort
+}
 
+// Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: port, with: router)
+
+// Start the Kitura runloop (this call never returns)
 Kitura.run()
