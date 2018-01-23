edb9fd7063cbece29148a2686c620e023549709e
Arnaud Manaranche <arnaud.mana@gmail.com>
2018-01-23 11:44:15 +0100

    end

--- Sources/beacall-api/main.swift
+++ Sources/beacall-api/main.swift
@@ -8,30 +8,40 @@ router.all("/", middleware: BodyParser(), StaticFileServer(path: "./Public"))
 router.get("/student1") { request, response, next in
     
     let data = [
-        "course": [
+        "info": [
+            [
+                "id": "1",
+                "username": "victor",
+                "password": "eemi2017"
+            ]
+        ],
+        "courses": [
             [
                 "place": "Les Panoramas",
                 "begin": "9h",
                 "end": "11h",
                 "lat": "48.870537",
                 "long": "2.342358",
-                "marge": "200"
+                "marge": "200",
+                "course": "Ruby on Rails"
             ],
             [
-                "place": "Les Panoramas",
+                "place": "Palais Brogniart",
                 "begin": "11h15",
                 "end": "13h15",
                 "lat": "48.870537",
                 "long": "2.342358",
-                "marge": "200"
+                "marge": "200",
+                "course": "Ruby on Rails"
             ],
             [
-                "place": "Les Panoramas",
+                "place": "Palais Brogniart",
                 "begin": "14h15",
                 "end": "16h15",
                 "lat": "48.870537",
                 "long": "2.342358",
-                "marge": "200"
+                "marge": "200",
+                "course": "Option design"
             ],
             [
                 "place": "Les Panoramas",
@@ -39,7 +49,8 @@ router.get("/student1") { request, response, next in
                 "end": "18h30",
                 "lat": "48.870537",
                 "long": "2.342358",
-                "marge": "200"
+                "marge": "200",
+                "course": "Anglais"
             ]
         ]
     ]
@@ -47,7 +58,30 @@ router.get("/student1") { request, response, next in
     try response.send(data).end()
 }
 
-// Resolve the port that we want the server to listen on.
+router.post("/login") { request, response, next in
+    
+    guard let body = request.body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    guard case .urlEncoded(let data) = body else {
+        try response.status(.badRequest).end()
+        return
+    }
+    
+    let username = data["username"]
+    
+    print(username ?? "User instance is nil")
+    
+    if (username == "victor") {
+        response.send("true")
+    } else {
+        response.send("false")
+    }
+    next()
+}
+
 let port: Int
 let defaultPort = 8080
 if let requestedPort = ProcessInfo.processInfo.environment["PORT"] {
@@ -56,8 +90,5 @@ if let requestedPort = ProcessInfo.processInfo.environment["PORT"] {
     port = defaultPort
 }
 
-// Add an HTTP server and connect it to the router
 Kitura.addHTTPServer(onPort: port, with: router)
-
-// Start the Kitura runloop (this call never returns)
 Kitura.run()
